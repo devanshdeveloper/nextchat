087cd6af21764981d0ca3e1e038f3b8758807ecf
devansh.developer <65781793+friendbro@users.noreply.github.com>
2023-04-04 15:22:18 +0530

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -6,21 +6,18 @@ export default function LandingPage() {
   return (
     <>
       <Head>
-        <title>Noter | A simple todo app built with Next.js and Firebase</title>
+        <title>NextChat</title>
       </Head>
       <div className="flex justify-around items-center h-[calc(100vh-48px)]">
         <div className="lg:w-[50vw] s:w-[80vw]">
           <div className="lg:text-4xl s:text-xl">
-            <span className="font-bold">Noter</span> is a simple, easy-to-use
+            <span className="font-bold">NextChat</span> is a simple, easy-to-use
           </div>
           <div className="lg:text-xl sm:text-lg mb-4 mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
-            quod id! Voluptatum itaque sunt, dolore officia praesentium
-            voluptatibus repellendus est? Quisquam provident magnam modi nemo
-            recusandae odit dolorem illum! Quo?
+          Say hello to a faster and more convenient way to stay in touch. Register now and unlock the full potential of our chat app.
           </div>
           <Link href="/auth">
-            <a className="btn"> Get Started </a>
+            <a className="btn">Register</a>
           </Link>
         </div>
         <div className="lg:block s:hidden">
